56aa345e33edcd87cd7501061c7c375b512f5c71
andrew gonzalez <planetman1125@gmail.com>
2026-03-30 09:41:31 -0400

    Initial Commit

---
+++ flukavike/flukavikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  flukavikeApp.swift
+//  flukavike
+//
+//  Created by andrew gonzalez on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct flukavikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
